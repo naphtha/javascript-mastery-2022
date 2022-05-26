4ad3902d9271aa3bc68500418fce8668a24a12a4
naphtha <naphthanewman@gmail.com>
2022-05-26 23:54:53 +0000

    added variables and naming conventions

---
+++ variable-naming.js
@@ -0,0 +1,30 @@
+/**
+ * Variables => enables us to store values
+ *
+ * Naming Convention
+ * 1. Use camelCase
+ * 2. Name can only contain numbers, underscores, letters and dollar sign ($)
+ * 3. Error will occur when using reserved keywords
+ * 4. Name cannot begin with a number
+ * 5. Do not start with an uppercase letter
+ * 6. Variables that are all in uppercase are reserved for constants that do not change
+ *
+ * NB: Variable names must be very descriptive
+ */
+
+// Using constants
+const PI = 3.142;
+
+// Descriptive variable names
+const firstJob = 'Lead Developer';
+const currentJob = 'Freelancer';
+
+console.log(currentJob);
+
+//Assignment
+
+const country = 'Ghana';
+const population = 3000000;
+const continent = 'Africa';
+
+console.log(country, continent, population);
