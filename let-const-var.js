7a2c05de79c3927bccc62a3ee69be87fb325977f
naphtha <naphthanewman@gmail.com>
2022-05-27 00:45:33 +0000

    added let-const-var

---
+++ let-const-var.js
@@ -0,0 +1,19 @@
+/**
+ * When do we use let
+ * ans: We use let keyword for a variable that can change later during the execution of the program
+ * let variables can undergo mutation
+ * Also useful for declaring empty variables
+ */
+
+/**
+ * When do we use const
+ * ans: Values that are not supposed to change
+ * This variables are immutable
+ * We cannot declare empty const variables
+ */
+
+/**
+ * By default always use const in variable naming and only use let when you sure that the value can change
+ */
+
+// var -> The old way to declaring variables prior to ES6
