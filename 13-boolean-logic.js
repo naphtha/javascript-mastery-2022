cea6764c502cf88397e75eefc95598764b8bc301
naphtha <naphthanewman@gmail.com>
2022-05-29 16:00:14 +0000

    added logigcal operators

---
+++ 13-boolean-logic.js
@@ -0,0 +1,19 @@
+/**
+ * Basic logical operators => AND, OR NOT operator
+ */
+
+/**
+ * AND -> returns true only id A and B are both true
+ * We can join more AND but only if all are true that it evaluates to true
+ */
+
+/**
+ * OR -> It will be true if only one of the variable is true
+ *
+ */
+
+let isVerified = false;
+
+if (!isVerified) {
+  console.log('Congrats');
+}
---
+++ 14-logical-operators.js
@@ -0,0 +1,5 @@
+const hasDriversLicense = true;
+const hasGoodVision = false;
+
+console.log(hasDriversLicense && hasGoodVision); // The AND operator -> both must be true to give true
+console.log(hasDriversLicense || hasGoodVision); // The OR operator -> even if one is true and the rest is false we get true
